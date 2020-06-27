5287eedafc8387b8af87551bf9b00327fb095ba8
Goryudyuma <umaretekyoumade@gmail.com>
2020-06-27 21:56:52 +0900

    abc001/B

--- atcoder/abc001/src/bin/b.rs
+++ atcoder/abc001/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        m:i32,
+    }
+    let ans = if m < 100 {
+        "00".to_string()
+    } else if m <= 5000 {
+        format!("{:02}", m / 100)
+    } else if m <= 30000 {
+        (m / 1000 + 50).to_string()
+    } else if m <= 70000 {
+        ((m / 1000 - 30) / 5 + 80).to_string()
+    } else {
+        "89".to_string()
+    };
+    println!("{}", ans)
 }
